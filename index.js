50cd49dc6756d4b917d42905223f197098eae42c
BALAND Jérome <baland.jerome@gmail.com>
2024-02-17 16:15:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// créer un form pour taper les todos
+
+// ajouter sur le dom ce qui est tapé dans l'input à la validation du form
+// monInput.value
+
+// supprimer un todo lorsque l'on clique dessus(.remove())
+
+
+// stocker dans le local storage la liste
+
+// consulter le local storage au lancement de l'application pour rajouter les todos
